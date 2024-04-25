766c1d9ded274bfaa71594dfe80d0d8763772c59
Yamil <invitado@MacBook-Pro-de-Invitado.local>
2024-04-25 07:31:16 -0600

    Improve logic

--- main.java
+++ main.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 class Main {
 
+  public static Scanner in = new Scanner(System.in);
+
   public static int card = 0;
+  public static int house = 0;
 
   static int m(int a, int b) {
     int max = a;
@@ -27,6 +30,7 @@ class Main {
     if ((res.toLowerCase()).equals("y")) {
       return true;
     } else if ((res.toLowerCase()).equals("n")) {
+      house = m2("y", house);
       return false;
     } else {
       System.err.println("Invalid response");
@@ -37,25 +41,53 @@ class Main {
   static int m2(String res, int card) {
     if (proveRes(res)) {
       int rand = m(10, 1);
+
       return card += rand;
     } else {
-      System.err.println("Ok!");
-      return 0;
+      return card;
     }
   }
 
+  static void game() {
+    System.out.println("You have " + card + ", want to continue? (Y/N)");
+    System.out.println("house: " + house);
+    String response = in.nextLine();
+    card = m2(response, card);
+    house = m2(response, house);
+  }
+
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
     int max = 10;
     int min = 1;
 
+    // Init game
     card = m(max, min);
-    System.out.println("You have " + card + " want to continue? (Y/N)");
-    String response = in.nextLine();
+    house = m(max, min);
 
-    card = m2(response, card);
+    // Init game
+    while (card <= 21) {
+      if (card > 21) {
+        System.out.println("You have " + card + ", You loose!");
+        System.exit(0);
+      } else if (house > 21) {
+        System.out.println("House have " + house + ", You win!");
+        System.exit(0);
+      }
 
-    System.out.println(card);
+      if (card == 21) {
+        System.out.println("You have " + card + ", You Win!");
+        System.exit(0);
+      } else {
+        game();
+      }
+    }
+
+    if (house == 21) {
+      System.out.println("House has: " + card + ", House wins!");
+      System.exit(0);
+    }
+
+    System.out.println("You have " + card + ", You Win!");
 
     in.close();
   }
